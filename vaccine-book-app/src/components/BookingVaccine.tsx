1a1509ce567a209ca01b9f30b596a85cf1a97391
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-31 20:48:36 +0700

    week 11 update

--- vaccine-book-app/src/components/BookingVaccine.tsx
+++ vaccine-book-app/src/components/BookingVaccine.tsx
@@ -36,7 +36,7 @@ export default function BookingVaccine() {
   };
 
   return (
-    <div className="bg-slate-100 rounded-lg space-x-5 space-y-6 w-4/5 px-10 py-5 justify-center">
+    <div className="bg-slate-100 rounded-lg space-x-5 space-y-6 w-4/5 px-10  justify-center mb-[30px]">
       <div className="space-y-4">
         <PersonalInput
           onFirstnameChange={(value: string) => {
@@ -50,33 +50,37 @@ export default function BookingVaccine() {
           }}
         />
 
-        <div className="space-y-0">
-          <SelectHospital
-            onLocationChange={(value: string) => {
-              setHospital(value);
-              console.log("hospital " + hospital);
-            }}
-          />
-        </div>
-        <div>
-          <p className="text-gray-600">Choose Date</p>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              className="bg-white"
-              value={reserverDate}
-              onChange={(val) => {
-                setReserveDate(val);
+        <div className="flex flex-row justify-evenly items-center">
+          <div className="space-y-0">
+            <SelectHospital
+              onLocationChange={(value: string) => {
+                setHospital(value);
+                console.log("hospital " + hospital);
               }}
             />
-          </LocalizationProvider>
+          </div>
+          <div>
+            <p className="text-gray-600">Choose Date</p>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                className="bg-white"
+                value={reserverDate}
+                onChange={(val) => {
+                  setReserveDate(val);
+                }}
+              />
+            </LocalizationProvider>
+          </div>
         </div>
       </div>
-      <button
-        className="block rounded-md bg-sky-500 hover:bg-sky-900 px-3 py-2 shadow-sm text-white"
-        onClick={makeReservation}
-      >
-        Booking Vaccine
-      </button>
+      <div className="items-center w-[100%] py-[60px]">
+        <button
+          className="block rounded-md bg-sky-500 hover:bg-sky-900 px-3 py-2 shadow-sm text-white w-[100%]"
+          onClick={makeReservation}
+        >
+          Confirm
+        </button>
+      </div>
     </div>
   );
 }
